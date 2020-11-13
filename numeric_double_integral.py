5b157dce0c85e151faf6af14471e8eed637cb84f
Daniel Modin <danielmodin.md@gmail.com>
2020-11-13 20:32:45 +0100

    added mid point sum

--- numeric_double_integral.py
+++ numeric_double_integral.py
@@ -19,8 +19,26 @@ def double_integral(func,a,b,c,d,n):
 		V=V+A*dy
 	return V
 
+#mid point sum. will converge faster
+def mid_double_integral(func,a,b,c,d,n):
+	dx = (b-a)/n
+	dy = (d-c)/n
+	V=0
+	for p in range(n):
+		yp = c+p*dy+0.5*dy
+		A=0
+		for i in range(n):
+			xi = a+i*dx+0.5*dx
+			A=A+func(xi,yp)*dx
+		V=V+A*dy
+	return V
+
+
+
 #check if works (verified against exact calculation)
-print(double_integral(f,1,2,1,2,5000))
+#see how much faster midpoint sum converges 
+print(double_integral(f,1,2,1,2,10))
+print(mid_double_integral(f,1,2,1,2,10)) #only n=10 slices, midpoint much better 
 
 
 	
